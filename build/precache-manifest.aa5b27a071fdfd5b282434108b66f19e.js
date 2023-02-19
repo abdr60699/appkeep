2afa6c44d79652a8947a60cfb65cbf0720a0dd7f
Abdul Rahman <abdr60699@gmail.com>
2023-02-19 23:42:40 +0530

    first commit

---
+++ build/precache-manifest.aa5b27a071fdfd5b282434108b66f19e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa2ab8a0ade5c5d2c728ac7acd66a701",
+    "url": "/Keeperapp/index.html"
+  },
+  {
+    "revision": "a862c478cdbb823256a9",
+    "url": "/Keeperapp/static/js/2.401cd8eb.chunk.js"
+  },
+  {
+    "revision": "d3370b57b25a77ef3d1a",
+    "url": "/Keeperapp/static/js/main.17c81308.chunk.js"
+  },
+  {
+    "revision": "ad3330e742edb1d84341",
+    "url": "/Keeperapp/static/js/runtime-main.1c925079.js"
+  }
+]);
